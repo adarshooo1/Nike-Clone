b57190d4d8bfbfe14d6f1de3c9a4eb9bc5daa2ac
Adarsh Singh <adarshsinghak001@gmail.com>
2023-05-03 21:45:26 +0530

    Nike Clone Day 3

--- components/MenuMobile.jsx
+++ components/MenuMobile.jsx
@@ -16,27 +16,31 @@ const subMenuData = [
     { id: 4, name: "Football shoes", doc_count: 107 },
 ];
 
-const Menu = ({ showCatMenu, setShowCatMenu }) => {
+const MenuMobile = ({ showCatMenu, setShowCatMenu , setMobileMenu}) => {
     return (
-        <ul className='hidden md:flex items-center gap-8 font-medium text-black'>
+        <ul className='flex flex-col md-hidden font-bold absolute top-[50px] left-0 w-full h-[calc(100vh-50px)] bg-white border-t text-black'>
             {data.map((item) => {
                 return (
                     <React.Fragment key={item.id}>
                         {item?.subMenu ? (
                             <li
-                                className="cursor-pointer flex items-center gap-2 relative"
-                                onMouseEnter={() => setShowCatMenu(true)}
-                                onMouseLeave={() => setShowCatMenu(false)}
+                                className="cursor-pointer flex py-4 px-5 border-b flex-col relative"
+                                onClick={()=>{setShowCatMenu(!showCatMenu)}}
                             >
+
+                                <div className='flex justify-between text-center'>
                                 {item.name}
                                 <BsChevronDown size={14} />
+                                </div>
+
+                                
 
                                 {showCatMenu && (
-                                    <ul className='bg-white absolute top-6 left-0 min-w-[250px] px-1 py-1 text-black shadow-lg'>
+                                    <ul className='bg-black/[0.05] -mx-5 mt-4 -mb-4'>
                                         {subMenuData.map((subMenu) => {
                                             return (
-                                                <Link key={subMenu.id} href="/" onClick={() => setShowCatMenu(false)}>
-                                                    <li className='h-12 flex justify-between items-center px-3 hover:bg-black/[0.03] rounded-md'>{subMenu.name}
+                                                <Link key={subMenu.id} href="/" onClick={() => {setShowCatMenu(false),setMobileMenu(false)}}>
+                                                    <li className='py-4 px-8 border-t flex justify-between'>{subMenu.name}
                                                         <span className="opacity-50 text-sm">78</span>
                                                     </li>
                                                 </Link>
@@ -46,8 +50,8 @@ const Menu = ({ showCatMenu, setShowCatMenu }) => {
                                 )}
                             </li>
                         ) : (
-                            <li className='cursor-pointer'>
-                                <Link href={item?.url}>{item.name} </Link>
+                            <li className='py-4 px-5 border-b'>
+                                <Link href={item?.url} onClick={()=>{setMobileMenu(false)}}>{item.name} </Link>
                             </li>
                         )}
                     </React.Fragment>
@@ -57,4 +61,4 @@ const Menu = ({ showCatMenu, setShowCatMenu }) => {
     );
 };
 
-export default Menu;
+export default MenuMobile;
